cb25249f4be20c93b25858657283b265e42abc5e
Lee Pedersen <leepedersenr@gmail.com>
2019-12-13 09:25:51 -0800

    add tests and functionality for anagram and antigram methods

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,10 +2,23 @@ class Words
   attr_accessor(:words)
 
   def initialize(word_a, word_b)
-    @words = [word_a, word_b]
+    @words = [word_a.downcase, word_b.downcase]
   end
 
   def anagram?()
-    false
+    if @words[0].chars.sort.join.eql? @words[1].chars.sort.join
+      true
+    else
+      false
+    end
+  end
+
+  def antigram?()
+    @words[0].each_char do |chr|
+      if @words[1].include?(chr)
+        return false
+      end
+    end
+    return true
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -5,10 +5,26 @@ describe('Word#anagram') do
     word = Words.new("cat", "chicken")
     expect(word.anagram?).to(eq(false))
   end
-  # it("returns true when given words are anagrams") do
-  #   word = Words.new("cat", "act")
-  #   expect(word.anagram?).to(eq(true))
-  # end
+  it("returns true when given words are anagrams") do
+    word = Words.new("resistance", "ancestries")
+    expect(word.anagram?).to(eq(true))
+  end
+  it("returns true when given words are anagrams, case insensitive") do
+    word = Words.new("Orchestra", "Carthorse")
+    expect(word.anagram?).to(eq(true))
+  end
+  # ignore white space
+end
+
+describe('Word#antigram') do
+  it("returns false when given words are not antigrams") do
+    word = Words.new("cat", "chicken")
+    expect(word.antigram?).to(eq(false))
+  end
+  it("returns true when given words are antigrams") do
+    word = Words.new("cat", "dog")
+    expect(word.antigram?).to(eq(true))
+  end
 end
 
 # describe('Word#antigram') do
