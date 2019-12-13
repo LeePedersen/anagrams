aa1d929beed2f75031e873200cf174c3fe490db4
Lee Pedersen <leepedersenr@gmail.com>
2019-12-13 08:57:11 -0800

    add first test and method to pass

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,2 +1,11 @@
-class Anagram
+class Words
+  attr_accessor(:words)
+
+  def initialize(word_a, word_b)
+    @words = [word_a, word_b]
+  end
+
+  def anagram?()
+    false
+  end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,4 +1,14 @@
 require 'anagram'
 
-describe('Anagram') do
+describe('Word#anagram') do
+  it("returns false when given words are not anagrams") do
+    word = Words.new("cat", "chicken")
+    expect(word.anagram?).to(eq(false))
+  end
+  # it("returns true when given words are anagrams") do
+  #   word = Words.new("cat", "act")
+  #   expect(word.anagram?).to(eq(true))
+  # end
 end
+
+# describe('Word#antigram') do
